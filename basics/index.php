ce0c4c6e06d375fed8fd860fc9b20b06819fc629
Artem_Lakatun <alakatuni@mail.ru>
2023-12-08 15:09:58 +0300

    feat: date();

--- basics/index.php
+++ basics/index.php
@@ -4,6 +4,18 @@
 ?>
 <h1>Главная</h1>
 <?php
-    echo date('m-l H:i:s');
-require "blocks/footer.php"
+function performOperation($callback) {
+    // Выполнение некоторой операции
+    echo "Выполняется операция...\n";
+
+    // Вызов колбэк функции
+    call_user_func($callback);
+}
+
+function myCallbackFunction() {
+    echo "Это моя колбэк функция!\n";
+}
+
+// Передача колбэк функции в качестве аргумента
+performOperation('myCallbackFunction');
 ?>
